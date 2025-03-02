bb6a9532dc62065fa3e65b9e880576b4c8007a58
vworn <jasonkmathewpersonal@gmail.com>
2025-03-02 16:28:01 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -21,3 +21,27 @@ const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails());
 book1.updateCopies(-1);
 console.log(book1.getDetails());
+
+// Task 2: Creating a Borrower Class
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = [];
+    }
+
+    borrowBook(bookTitle) {
+        this.borrowedBooks.push(bookTitle);
+    }
+
+    returnBook(bookTitle) {
+        this.borrowedBooks = this.borrowedBooks.filter(title => title !== bookTitle);
+    }
+}
+
+// Test cases for Task 2
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
