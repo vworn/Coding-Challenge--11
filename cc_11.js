cbf88dac7df598b287e970835d9f3997a214f97f
vworn <jasonkmathewpersonal@gmail.com>
2025-03-02 16:28:33 -0500

    Task 3 - Created Library Class

--- cc_11.js
+++ cc_11.js
@@ -45,3 +45,29 @@ borrower1.borrowBook("The Great Gatsby");
 console.log(borrower1.borrowedBooks);
 borrower1.returnBook("The Great Gatsby");
 console.log(borrower1.borrowedBooks);
+
+// Task 3: Creating a Library Class
+class Library {
+    constructor() {
+        this.books = [];
+        this.borrowers = [];
+    }
+
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    listBooks() {
+        this.books.forEach(book => console.log(book.getDetails()));
+    }
+
+    addBorrower(borrower) {
+        this.borrowers.push(borrower);
+    }
+}
+
+// Test cases for Task 3
+const library = new Library();
+library.addBook(book1);
+library.listBooks();
+library.addBorrower(borrower1);
